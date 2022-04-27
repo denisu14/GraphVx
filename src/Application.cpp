d596fbda03cf3821ddbd7484dfd89b6b44a9b426
denisu14 <denizyilmaz3721@gmail.com>
2022-04-27 19:19:35 +0300

    gitignore

--- src/Application.cpp
+++ src/Application.cpp
@@ -23,6 +23,13 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
